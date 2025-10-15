1ed8523c6558a8977339a98f2df36e6df91799cf
Djapedjape123 <petafipedjaradic@gmail.com>
2025-10-15 23:38:24 +0200

    Error za duoku verifikaciju

--- controllers/auth/verify.js
+++ controllers/auth/verify.js
@@ -1,21 +1,33 @@
-const UserModel = require("../../models/UserModel")
+const UserModel = require("../../models/UserModel");
 
-const verify = async(req,res,next) => {
+const verify = async (req, res, next) => {
   try {
-     let verifyUser = await UserModel.updateOne({_id:req.params.code},{$set:{active:true}});
-     if (verifyUser.modifiedCount ===1) {
-          res.redirect("/auth/login");
-        
-     }else if(verifyUser.modifiedCount ===1 && verifyUser.matchedCount ===1){
-         next(new Error("vec ste se verifikovali"))
+    
+    const verifyUser = await UserModel.updateOne(
+      { _id: req.params.code, active: false },
+      { $set: { active: true } }
+    );
 
-    }else{
-        
-        next(new Error("DOSLO JE DO GRESKE eMAIL NIJE VERIFIKOVAN"))
-     }
-  } catch (error){
-    next(error)
+    if (verifyUser.modifiedCount === 1) {
+      
+      return res.redirect("/auth/login");
+    }
+
+    
+    const alreadyActive = await UserModel.findOne({
+      _id: req.params.code,
+      active: true,
+    });
+
+    if (alreadyActive) {
+      return next(new Error("Vec ste se verifikovali"));
+    }
+
+   
+    return next(new Error("Doslo je do greske, email nije verifikovan"));
+  } catch (error) {
+    next(error);
   }
-}
+};
 
-module.exports = verify
+module.exports = verify;
