f9ccef02fe2abd27b0040b90ae0f53c98402c8dc
Djapedjape123 <petafipedjaradic@gmail.com>
2025-10-20 19:19:44 +0200

    KONACNO2

--- controllers/event/galleryView.js
+++ controllers/event/galleryView.js
@@ -4,15 +4,15 @@ const UserModel = require("../../models/UserModel");
 
 const galleryView = async (req, res) => {
     try {
-        
+
         const events = await EventModel.find();
         const users = await UserModel.find();
-          const userMap = {};
-    users.forEach(u => {
-      userMap[u._id.toString()] = u;
-    });
+        const userMap = {};
+        users.forEach(u => {
+            userMap[u._id.toString()] = u;
+        });
 
-        res.render("event/galleryView", { user: req.session.user,users,userMap, event: events, dayjs });
+        res.render("event/galleryView", { user: req.session.user, users, userMap, event: events, dayjs });
 
     } catch (error) {
         res.status(500).send(error.message);
