7650ee2269e14bebd534cc6288e0dafeb3bc9ec3
zile028 <zile028@gmail.com>
2025-10-20 21:37:28 +0200

    pagination

--- controllers/event/galleryView.js
+++ controllers/event/galleryView.js
@@ -3,16 +3,38 @@ const dayjs = require("dayjs");
 const UserModel = require("../../models/UserModel");
 
 const galleryView = async (req, res) => {
+
+    let page = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
+    let limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : 3;
+
+    let offset = (page - 1) * limit;
     try {
+        const events = await EventModel.aggregate([
+            {$skip: offset},
+            {$limit: limit},
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                firstName: 1,
+                                lastName: 1,
+                                avatar: 1
+                            }
+                        }
+                    ]
+                }
+            },
+            {$unwind: "$user"}
 
-        const events = await EventModel.find();
-        const users = await UserModel.find();
-        const userMap = {};
-        users.forEach(u => {
-            userMap[u._id.toString()] = u;
-        });
+        ]);
 
-        res.render("event/galleryView", { user: req.session.user, users, userMap, event: events, dayjs });
+        res.render("event/galleryView", {user: req.session.user, events: events, dayjs});
 
     } catch (error) {
         res.status(500).send(error.message);
