fd3e94812554541223c12e7c4ce9666168cf3870
DEJAN ZIVKOVIC <zile028@gmail.com>
2025-10-21 08:38:20 +0200

    fix create event

--- controllers/event/createEvent.js
+++ controllers/event/createEvent.js
@@ -5,67 +5,64 @@ const MB = 1024 * 1024;
 const validType = ["image/png", "image/jpg", "image/jpeg"];
 
 const createEvent = async (req, res) => {
-    try {
-        const errors = {};
-        const user = req.session.user ? req.session.user._id : undefined;
-        const {title, description, startAt, endAt} = req.body;
-        const file = req.files?.thumbnail;
+	try {
 
-        // Provera obaveznih polja
-        if (!title || !description || !user || !startAt || !endAt) {
-            return res.send({message: "Sva polja su obavezna!"});
-        }
+		const errors = {};
+		const user = req.session.user ? req.session.user._id : undefined;
+		const { title, description, startAt, endAt } = req.body;
+		const file = req.files?.thumbnail;
 
-        if (!title || title.length < 5) {
-            errors.title = "Naslov mora imati najmanje 5 karaktera";
-        }
+		// Provera obaveznih polja
+		if (!title || !description || !user || !startAt || !endAt) {
+			return res.send({ message: "Sva polja su obavezna!" });
+		}
 
-        if (!decription || decription.length < 10) {
-            errors.description = "Opis mora imati najmanje 10 karaktera";
-        }
-        if (!user) {
-            errors.user = "Morate izabrati autora";
-        }
-        if (!startAt) {
-            errors.startAt = "Morate uneti početak događaja";
-        }
-        if (!endAt) {
-            errors.endAt = "Morate uneti kraj događaja";
-        }
+		if (!title || title.length < 5) {
+			errors.title = "Naslov mora imati najmanje 5 karaktera";
+		}
 
-        // Provera fajla
-        if (!req.files || !req.files.thumbnail) {
-            return res.send({message: "Naslovna slika je obavezna!"});
-        }
+		if (!description || description.length < 10) {
+			errors.description = "Opis mora imati najmanje 10 karaktera";
+		}
+		if (!user) {
+			errors.user = "Morate izabrati autora";
+		}
+		if (!startAt) {
+			errors.startAt = "Morate uneti početak događaja";
+		}
+		if (!endAt) {
+			errors.endAt = "Morate uneti kraj događaja";
+		}
+		// Provera fajla
+		if (!req.files || !req.files.thumbnail) {
+			return res.send({ message: "Naslovna slika je obavezna!" });
+		}
+		console.log("GRESKA VALIDACIJA", errors);
 
-        const thumbnailFile = req.files.thumbnail;
-        const extension = path.extname(thumbnailFile.name);
-        const storeName = new Date().getTime().toString() + extension;
-        const uploadPath = path.join(
-            __dirname,
-            "../../public/uploads/",
-            storeName
-        );
-        await thumbnailFile.mv(uploadPath);
+		const thumbnailFile = req.files.thumbnail;
+		const extension = path.extname(thumbnailFile.name);
+		const storeName = new Date().getTime().toString() + extension;
+		const uploadPath = path.join(__dirname, "../../public/uploads/", storeName);
+		await thumbnailFile.mv(uploadPath);
 
-        // Kreiranje eventa
-        const event = new EventModel({
-            title,
-            thumbnail: "/uploads/" + storeName,
-            description,
-            user,
-            startAt,
-            endAt
-        });
-        await event.save();
-        res.redirect("/event");
-    } catch (error) {
-        res.render("event/createView", {
-            message: "Greška pri kreiranju događaja!",
-            user: req.session.user
-        });
-    }
+		// Kreiranje eventa
+		const event = new EventModel({
+			title,
+			thumbnail: "/uploads/" + storeName,
+			description,
+			user,
+			startAt,
+			endAt,
+		});
+		await event.save();
+		res.redirect("/event");
+	} catch (error) {
+		console.log("GRESKA CREATE", error);
+		res.render("event/createView", {
+			message: "Greška pri kreiranju događaja!",
+			user: req.session.user,
+		});
+	}
 };
-
 
 module.exports = createEvent;
--- controllers/event/listEvents.js
+++ controllers/event/listEvents.js
@@ -2,27 +2,28 @@ const EventModel = require("../../models/EventModel");
 const UserModel = require("../../models/UserModel");
 
 const listEvents = async (req, res) => {
-  try {
-    // Pronađi sve evente i popuni podatke o korisniku
-    const events = await EventModel.find().sort({ createdAt: -1 });
-    // Dohvati korisnike za svaki event
-    const eventsWithUser = await Promise.all(
-      events.map(async (event) => {
-        let userName = "Nepoznat korisnik";
-        if (event.user) {
-          const user = await UserModel.findById(event.user);
-          if (user) userName = user.firstName + " " + user.lastName;
-        }
-        return { ...event._doc, userName };
-      })
-    );
-    res.render("event/listView", {
-      events: eventsWithUser,
-      user: req.session.user,
-    });
-  } catch (error) {
-    res.send({ message: "Greška pri učitavanju događaja!", error });
-  }
+	try {
+		// Pronađi sve evente i popuni podatke o korisniku
+		const events = await EventModel.find().sort({ createdAt: -1 });
+		console.log(events);
+		// Dohvati korisnike za svaki event
+		const eventsWithUser = await Promise.all(
+			events.map(async (event) => {
+				let userName = "Nepoznat korisnik";
+				if (event.user) {
+					const user = await UserModel.findById(event.user);
+					if (user) userName = user.firstName + " " + user.lastName;
+				}
+				return { ...event._doc, userName };
+			})
+		);
+		res.render("event/listView", {
+			events: eventsWithUser,
+			user: req.session.user,
+		});
+	} catch (error) {
+		res.send({ message: "Greška pri učitavanju događaja!", error });
+	}
 };
 
 module.exports = listEvents;
